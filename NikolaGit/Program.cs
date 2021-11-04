c1704631d27d78c577bc6f7fcf87611f4a9562d0
vladik1337228 <life.enot@mail.ru>
2021-11-04 20:25:42 +0200

    Signed-off-by: vladik1337228 <life.enot@mail.ru>

--- NikolaGit/Program.cs
+++ NikolaGit/Program.cs
@@ -7,6 +7,14 @@ namespace NikolaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
